99f994e02076f705fd3a4227509b05f9bdce3261
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-07-22 17:23:12 -0600

    Created imagebox component and added it to recipe-showcase component

--- src/pages/recipe-showcase/recipe-showcase.component.jsx
+++ src/pages/recipe-showcase/recipe-showcase.component.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { Paper, Typography, Box, Grid, Divider} from '@mui/material';
 import InstructionsBox from '../../components/instructions-box/instructions-box.component';
 import IngredientsBox from '../../components/ingredients-box/ingredients-box.component';
+import ImageBox from '../../components/image-box/image-box.component';
 
 const RecipeShowcase = () => {
   const {recipes} = useContext(RecipesContext);
@@ -18,12 +19,12 @@ const RecipeShowcase = () => {
   const { title, ingredients, image, instructions } = recipe;
 
   return (
-    <Paper elevation={5} sx={{ backgroundColor: 'bisque' }}>
+    <Paper elevation={5} sx={{ backgroundColor: '#54676' }}>
       <Grid container>
         {/* First Row */}
         <Grid item xs={12} sm={6}>
-          <Box p={2}>
-            <img src={image} alt={title} style={{ maxWidth: '100%', height: 'auto' }} />
+          <Box margin={2}>
+              <ImageBox image={image}/>
           </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
