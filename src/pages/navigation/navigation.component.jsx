be7a9a7ca5add539be40d59d5f9622ccc449bd4e
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-08-09 17:48:58 -0600

    Added some styling to the navbar

--- src/pages/navigation/navigation.component.jsx
+++ src/pages/navigation/navigation.component.jsx
@@ -1,48 +1,58 @@
-import { Fragment, useContext} from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase-utils";
 import { capitalizeFirstLetter } from "../../utils/utils";
 
-
 const Navigation = () => {
-    const userCredential = useContext(UserContext);
-    const {user} = userCredential;
+  const userCredential = useContext(UserContext);
+  const { user } = userCredential;
 
-
-    return (
-        <Fragment> {/* Contains the entire page */}
-
-            <div>
-                <Box display={'flex'} justifyContent={'flex-end'} sx={{ backgroundColor: '#FCDDBC' }}>
-                    <Box mr={2}>
-                        <Link to="/">
-                            Home
-                        </Link>    
-                    </Box>
-                    <Box mr={2}>
-                        <Link to="/recipe/upload">
-                            Upload Recipe
-                        </Link>
-                    </Box>
-                    <Box mr={2}>
-                        {user && user.displayName? 
-                            <span> {capitalizeFirstLetter(user.displayName)}, 
-                                <Link as='span'onClick={signOutUser}>
-                                    Sign Out
-                                </Link>
-                            </span> : 
-                            <Link to="/auth">
-                                Log In
-                            </Link>
-                        }
-                    </Box>
-                </Box>
-            </div>
-            <Outlet /> {/* Renders child routes */}
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <div>
+        <Box
+          p={1}
+          display={"flex"}
+          justifyContent={"flex-end"}
+          alignItems={"center"}
+          sx={{
+            backgroundColor: "#517664",
+            boxShadow: "0px 0px 2px rgba(0,0,0,0.1)"
+          }}
+        >
+          <Box mr={2}>
+            <Typography variant="body1">
+              <Link to="/">Home</Link>
+            </Typography>
+          </Box>
+          <Box mr={2}>
+            <Typography variant="body1">
+              <Link to="/recipe/upload">Upload Recipe</Link>
+            </Typography>
+          </Box>
+          <Box mr={2}>
+            {user && user.displayName ? (
+              <Typography as="span" sx={{ color: "#db9d47" }}>
+                <Link to="/profile">
+                {capitalizeFirstLetter(user.displayName)}, &nbsp;
+                </Link>
+                <Link as="span" onClick={signOutUser}>
+                  Sign Out
+                </Link>
+              </Typography>
+            ) : (
+              <Typography variant="body1">
+                <Link to="/auth">Log In</Link>
+              </Typography>
+            )}
+          </Box>
+        </Box>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Navigation;
