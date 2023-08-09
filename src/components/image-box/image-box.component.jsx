5a4d6e0f6dae868fcc890a5d90e68b73f56ff7e8
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-08-09 17:48:15 -0600

    Ensured image would be centered in xs size

--- src/components/image-box/image-box.component.jsx
+++ src/components/image-box/image-box.component.jsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 
 const ImageBox = ({ image }) => {
   return (
-    <Box> 
+    <Box display="flex" justifyContent="center" alignItems="center" height="100%">
       <img src={image} alt="Recipe" style={{ maxWidth: '100%', height: 'auto', borderRadius:'.5em', border:'.5em solid white'}} />
     </Box>
   );
