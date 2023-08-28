39784ee336cbeb8437fd42f9c7d38d150d88f1cd
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-08-28 15:23:40 -0600

    Added script to initialize express with yarn. Set up an upload point for the client to send files to.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
-const express = require('express');
+import express from 'express';
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+
+
+// Initialize cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'some_folder_name',
+    format: async (req, file) => 'png', // supports promises as well
+  },
+});
+
+const parser = multer({ storage: storage });
+
 const app = express();
 const port = 3001;
 
 app.get('/api', (req, res) => {
   res.send('Hello from the API!');
+});
+
+// New upload endpoint
+app.post('/api/upload', parser.single('image'), (req, res) => {
+  res.json({ file: req.file });
 });
 
 app.listen(port, () => {
