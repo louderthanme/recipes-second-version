47c4881d017f1bea89d51dfd961fb90019007e7a
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-09-10 17:43:49 -0600

    Added a title to home component

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,18 +1,18 @@
 import { useContext } from 'react';
 import { RecipesContext } from '../../contexts/recipe.context';
-import { Box } from '@mui/system';
+import { Box, Typography } from '@mui/material'; // Import Typography from Material-UI
 import Carousel from '../../components/Carousel/recipes-carousel/recipes-carousel.component';
 
 const Home = () => {
 
-  const {recipes} = useContext(RecipesContext);
+  const { recipes } = useContext(RecipesContext);
 
   return (
     <Box marginTop={4} style={{ width: '80%', height: '400px', margin: '0 auto' }}>
-      <Carousel  recipes={recipes}/>
+      <Typography variant="h2" gutterBottom ml={26} mb={6}> Featured Recipes</Typography>
+      <Carousel recipes={recipes} />
     </Box>
   );
- 
 };
 
 export default Home;
