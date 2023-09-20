814419151a85830979b4a59ed2996a58ad6d9bf9
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-09-20 09:00:33 -0600

    Modified width of carousel loading screen to 82%

--- src/components/ui/loading-screens/carousel-loading.component.jsx
+++ src/components/ui/loading-screens/carousel-loading.component.jsx
@@ -2,7 +2,7 @@ import { Skeleton, Paper, Box } from "@mui/material";
 
 const CarouselLoading = () => {
     return (
-        <Paper elevation={4} sx={{ width: '70%', margin: '0 auto', padding: '20px', backgroundColor: '#66101F' }}>
+        <Paper elevation={4} sx={{ width: '82%', margin: '0 auto', padding: '20px', backgroundColor: '#c3bcfc' }}>
           {/* Simulating 3 CarouselPolaroids in the loading state */}
           {[0].map((index) => (
             <Box key={index} sx={{ padding: '0 5px' }}>
