c12945715588edcc6c49e4d4e3134b869fa87cbb
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-10-09 19:02:34 -0600

    Same sized authentication components

--- src/pages/authentication/authentication.component.jsx
+++ src/pages/authentication/authentication.component.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import SignInForm from '../../components/Authentication/sign-in-form/sign-in-form.component';
 import SignUpForm from '../../components/Authentication/sign-up-form/sign-up-form.component';
@@ -6,30 +6,35 @@ import SnackbarFormMessage from '../../components/ui/snackbar-form-message/snack
 
 import { useSnackbar } from '../../hooks/useSnackbar';
 
+import Box from '@mui/material/Box';
 
 const AuthPage = () => {
-    const [showSignUp, setShowSignUp] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
 
-    const [snackbar, showSnackbar, hideSnackbar] = useSnackbar();
+  const [snackbar, showSnackbar, hideSnackbar] = useSnackbar();
 
-    const switchToSignUp = () => setShowSignUp(true);
-    const switchToSignIn = () => setShowSignUp(false);
+  const switchToSignUp = () => setShowSignUp(true);
+  const switchToSignIn = () => setShowSignUp(false);
 
-    return (
-        <>
-            {showSignUp ? <SignUpForm switchToSignIn={switchToSignIn} showSnackbar={showSnackbar} /> 
-            : <SignInForm switchToSignUp={switchToSignUp} showSnackbar={showSnackbar} />}
-            
-            <SnackbarFormMessage 
-                open={snackbar.open}
-                message={snackbar.message}
-                severity={snackbar.severity}
-                position={{ vertical: 'top', horizontal: 'center' }}
-                onClose={hideSnackbar}
-            />
-        
-        </>
-    );
+  return (
+    <Box display="flex" justifyContent="center">
+      <Box width="400px">
+        {showSignUp ? (
+          <SignUpForm switchToSignIn={switchToSignIn} showSnackbar={showSnackbar} />
+        ) : (
+          <SignInForm switchToSignUp={switchToSignUp} showSnackbar={showSnackbar} />
+        )}
+      </Box>
+
+      <SnackbarFormMessage
+        open={snackbar.open}
+        message={snackbar.message}
+        severity={snackbar.severity}
+        position={{ vertical: 'top', horizontal: 'center' }}
+        onClose={hideSnackbar}
+      />
+    </Box>
+  );
 };
 
 export default AuthPage;
