3ed3f129af89ea602d8610fe82a6c7ee1adc0f79
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-11-07 19:56:46 -0600

    Deploy to vercel

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const app = express();
 app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
